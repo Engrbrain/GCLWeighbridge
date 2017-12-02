d2c8fafa72eea89dacb058d92a3c2deeaf43cdde
Engrbrain <Engrbrain@gmail.com>
2017-12-02 16:26:19 +0100

    Project Created, First Commit

---
+++ SAP_GCL/SAP_GCL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAP_GCL.Startup))]
+namespace SAP_GCL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
